af11bb2fb0777eb0355aa5629c9753a0f15193a4
vyevt <95049761+vyevt@users.noreply.github.com>
2022-07-26 18:17:37 +0300

    Add files via upload

---
+++ docs/537.939c712f9105c85c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkangular_client=self.webpackChunkangular_client||[]).push([[537],{2537:(_,l,n)=>{n.r(l),n.d(l,{StoreModule:()=>r});var u=n(6895),e=n(487),t=n(8256);let r=(()=>{class o{}return o.\u0275fac=function(M){return new(M||o)},o.\u0275mod=t.oAB({type:o}),o.\u0275inj=t.cJS({imports:[u.ez,e.C]}),o})()}}]);
